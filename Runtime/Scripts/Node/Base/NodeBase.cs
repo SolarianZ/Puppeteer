c2524236a29fbbaf68a7aa26ae37019321362d2e
zqy <VDerGoW@hotmail.com>
2022-10-23 21:07:07 +0800

    feat: Runtime Node Interfaces

--- Runtime/Scripts/Node/Base/NodeBase.cs
+++ Runtime/Scripts/Node/Base/NodeBase.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace GBG.AnimationGraph.Node
 {
     [Serializable]
     public abstract class NodeBase
     {
+        #region Serialization Data
+
         public string Guid => _guid;
 
         [SerializeField]
         private string _guid;
 
-        
+
 #if UNITY_EDITOR
         public Vector2 EditorPosition
         {
@@ -23,10 +26,24 @@ namespace GBG.AnimationGraph.Node
         private Vector2 _editorPosition;
 #endif
 
+        #endregion
+
+
+        #region Runtime Properties
+
+        // internal abstract Playable AnimationPlayable { get; }
+
+        #endregion
+
 
         protected NodeBase(string guid)
         {
             _guid = guid;
+        }
+
+
+        public void Initialize(PlayableGraph playableGraph)
+        {
         }
     }
 }
