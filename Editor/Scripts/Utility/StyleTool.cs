fb6ab7c905393687858008db6b6313cc13b32778
zqy <vdergow@hotmail.com>
2022-11-01 17:21:37 +0800

    feat: Style Tool

---
+++ Editor/Scripts/Utility/StyleTool.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace GBG.AnimationGraph.Editor.Utility
+{
+    public static class StyleTool
+    {
+        public static StyleSheet GetCommonLightStyleSheet()
+        {
+            var getCommonLightStyleSheetMethod = GetUIElementsEditorUtilityMethod("GetCommonLightStyleSheet");
+            var commonLightStyleSheet = (StyleSheet)getCommonLightStyleSheetMethod!.Invoke(null, null);
+
+            return commonLightStyleSheet;
+        }
+
+        public static StyleSheet GetCommonDarkStyleSheet()
+        {
+            var getCommonDarkStyleSheetMethod = GetUIElementsEditorUtilityMethod("GetCommonDarkStyleSheet");
+            var commonDarkStyleSheet = (StyleSheet)getCommonDarkStyleSheetMethod!.Invoke(null, null);
+
+            return commonDarkStyleSheet;
+        }
+
+        public static void ForceDarkStyleSheet(VisualElement element)
+        {
+            if (EditorGUIUtility.isProSkin)
+            {
+                return;
+            }
+
+            var forceDarkStyleSheetMethod = GetUIElementsEditorUtilityMethod("ForceDarkStyleSheet");
+            forceDarkStyleSheetMethod!.Invoke(null, new object[] { element });
+        }
+
+
+        private static MethodInfo GetUIElementsEditorUtilityMethod(string methodName)
+        {
+            var uieEditorUtilType = typeof(Toolbar).Assembly
+                .GetType("UnityEditor.UIElements.UIElementsEditorUtility");
+            var methodInfo = uieEditorUtilType.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic);
+
+            return methodInfo;
+        }
+    }
+}
