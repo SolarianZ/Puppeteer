420ed2196f743c5e03b43b5c3ddd0eba7973bac3
zqy <vdergow@hotmail.com>
2022-11-01 19:37:39 +0800

    feat: Curve Tool

---
+++ Editor/Scripts/Utility/CurveTool.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace GBG.AnimationGraph.Editor.Utility
+{
+    public static class CurveTool
+    {
+        public static void Normalize(this AnimationCurve curve)
+        {
+            if (curve.IsConstantCurve())
+            {
+                curve.ClearKeys();
+                curve.AddKey(new Keyframe(0, 0, 0, 1));
+                curve.AddKey(new Keyframe(1, 1, 1, 0));
+                return;
+            }
+
+            var startKey = curve[0];
+            var endKey = curve[curve.length - 1];
+            var timeOffset = startKey.time - 0f;
+            var timeScale = 1.0f / (endKey.time - startKey.time);
+            var valueOffset = startKey.value - 0f;
+            var valueScale = 1.0f / (endKey.value - startKey.value);
+            var keys = curve.keys;
+            for (int i = 0; i < keys.Length; i++)
+            {
+                ref var key = ref keys[i];
+                key.time = (key.time - timeOffset) * timeScale;
+                key.value = (key.value - valueOffset) * valueScale;
+            }
+
+            curve.keys = keys;
+        }
+
+
+        public static void ClearKeys(this AnimationCurve curve)
+        {
+            for (int i = curve.length - 1; i >= 0; i--)
+            {
+                curve.RemoveKey(i);
+            }
+        }
+
+        public static bool IsConstantCurve(this AnimationCurve curve)
+        {
+            if (curve.length == 0)
+            {
+                return true;
+            }
+
+            var value = curve[0].value;
+            for (int i = 1; i < curve.length; i++)
+            {
+                if (!Mathf.Approximately(value, curve[i].value))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
