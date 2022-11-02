00c8204ced7517151daf7d21d1ab4c0c12a8a953
zqy <vdergow@hotmail.com>
2022-11-02 17:39:28 +0800

    feat: AnimationScriptNode

--- Runtime/Scripts/Node/MixerGraphNode/AnimationScriptNode.cs
+++ Runtime/Scripts/Node/MixerGraphNode/AnimationScriptNode.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using GBG.AnimationGraph.Component;
+using GBG.AnimationGraph.Graph;
 using GBG.AnimationGraph.Parameter;
 using UnityEngine;
 using UnityEngine.Animations;
@@ -11,10 +12,8 @@ namespace GBG.AnimationGraph.Node
 {
     public abstract class AnimationScriptAsset : ScriptableObject
     {
-        public abstract string Tag { get; }
-
-
-        public abstract AnimationScriptPlayable CreatePlayable(Animator animator, Skeleton skeleton);
+        public abstract AnimationScriptPlayable CreatePlayable(Skeleton skeleton,
+            PlayableGraph playableGraph, int inputCount);
     }
 
     [Serializable]
@@ -48,6 +47,8 @@ namespace GBG.AnimationGraph.Node
 
         private string[] _inputGuids;
 
+        private ParamInfo[] _runtimeInputWeightParams;
+
         #endregion
 
 
@@ -69,14 +70,63 @@ namespace GBG.AnimationGraph.Node
             return _inputGuids;
         }
 
+        protected internal override void InitializeConnection(IReadOnlyDictionary<string, GraphLayer> graphGuidTable,
+            IReadOnlyDictionary<string, NodeBase> nodeGuidTable)
+        {
+            base.InitializeConnection(graphGuidTable, nodeGuidTable);
+
+            for (int i = 0; i < MixerInputs.Count; i++)
+            {
+                Playable.SetInputWeight(i, GetInputWeight(i));
+            }
+        }
+
         protected internal override void PrepareFrame(float deltaTime) => throw new NotImplementedException();
 
 
-        protected override void InitializeParams(IReadOnlyDictionary<string, ParamInfo> paramGuidTable) =>
-            throw new NotImplementedException();
+        protected override void InitializeParams(IReadOnlyDictionary<string, ParamInfo> paramGuidTable)
+        {
+            // Input weights
+            _runtimeInputWeightParams = new ParamInfo[MixerInputs.Count];
+            for (int i = 0; i < _runtimeInputWeightParams.Length; i++)
+            {
+                var index = i;
+                var weightParam = MixerInputs[index].InputWeightParam;
+                if (!weightParam.IsValue)
+                {
+                    var runtimeInputWeightParam = paramGuidTable[weightParam.Guid];
+                    _runtimeInputWeightParams[index] = runtimeInputWeightParam;
 
-        protected override Playable CreatePlayable(PlayableGraph playableGraph) => throw new NotImplementedException();
+                    runtimeInputWeightParam.OnValueChanged += p => OnInputWeightChanged(index, p.GetFloat());
+                }
+            }
+        }
 
-        protected override float GetInputWeight(int inputIndex) => throw new NotImplementedException();
+        protected override Playable CreatePlayable(PlayableGraph playableGraph)
+        {
+            if (!ScriptAsset)
+            {
+                return Playable.Null;
+            }
+
+            // TODO: Need Skeleton argument.
+            var playable = ScriptAsset.CreatePlayable(null, playableGraph, MixerInputs.Count);
+            return playable;
+        }
+
+        protected override float GetInputWeight(int inputIndex)
+        {
+            var runtimeInputWeightParam = _runtimeInputWeightParams[inputIndex];
+            if (runtimeInputWeightParam != null)
+            {
+                return runtimeInputWeightParam.GetFloat();
+            }
+
+            return MixerInputs[inputIndex].InputWeightParam.GetFloat();
+        }
+
+
+        // TODO: OnInputWeightChanged
+        private void OnInputWeightChanged(int index, float weight) => throw new NotImplementedException();
     }
 }
